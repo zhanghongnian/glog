e44d0abfb6b4223c4c076a5cc259427debd9e6b3
jsharkc <1773867204@qq.com>
2018-04-01 22:29:42 +0800

    Add judgement

--- glog.go
+++ glog.go
@@ -667,8 +667,11 @@ func (buf *buffer) someDigits(i, d int) int {
 
 func (l *loggingT) println(s severity, args ...interface{}) {
 	buf, file, line := l.header(s, 0)
-	l.filter(tprintln, buf, "", args...)
-	// fmt.Fprintln(buf, args...)
+	if l.filterCard || l.filterIdentity {
+		l.filter(tprintln, buf, "", args...)
+	} else {
+		fmt.Fprintln(buf, args...)
+	}
 	l.output(s, buf, file, line, false)
 }
 
@@ -678,8 +681,11 @@ func (l *loggingT) print(s severity, args ...interface{}) {
 
 func (l *loggingT) printDepth(s severity, depth int, args ...interface{}) {
 	buf, file, line := l.header(s, depth)
-	l.filter(tprintf, buf, "", args...)
-	// fmt.Fprint(buf, args...)
+	if l.filterCard || l.filterIdentity {
+		l.filter(tprintf, buf, "", args...)
+	} else {
+		fmt.Fprint(buf, args...)
+	}
 	if buf.Bytes()[buf.Len()-1] != '\n' {
 		buf.WriteByte('\n')
 	}
@@ -688,8 +694,11 @@ func (l *loggingT) printDepth(s severity, depth int, args ...interface{}) {
 
 func (l *loggingT) printf(s severity, format string, args ...interface{}) {
 	buf, file, line := l.header(s, 0)
-	l.filter(tprintln, buf, format, args...)
-	// fmt.Fprintf(buf, format, args...)
+	if l.filterCard || l.filterIdentity {
+		l.filter(tprintln, buf, format, args...)
+	} else {
+		fmt.Fprintf(buf, format, args...)
+	}
 	if buf.Bytes()[buf.Len()-1] != '\n' {
 		buf.WriteByte('\n')
 	}
